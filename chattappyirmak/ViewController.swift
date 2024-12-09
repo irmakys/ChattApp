83912f8610f543ac4edf02de620cf3bab13dd796
Ramazan Gökmen <ramazangokmen@aol.com>
2024-12-09 15:27:27 +0300

    Initial Commit

---
+++ chattappyirmak/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chattappyirmak
+//
+//  Created by Ramazan Gökmen on 9.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ chattappyirmakTests/chattappyirmakTests.swift
@@ -0,0 +1,17 @@
+//
+//  chattappyirmakTests.swift
+//  chattappyirmakTests
+//
+//  Created by Ramazan Gökmen on 9.12.2024.
+//
+
+import Testing
+@testable import chattappyirmak
+
+struct chattappyirmakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
